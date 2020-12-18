aa7389afac74e8b6148a75bc50f6b8d468601788
bo-wang <bo.wang@rea-group.com>
2020-12-18 09:51:31 +0800

    Implement home page preliminary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import logo from './logo.png';
+import keymap from './keymap.png';
+import script from './script.png';
+import tools from './tools.png';
+
 import './App.css';
+import Card from "./Card";
+import { Row, Col } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header id="logo-header">
+          <Card image={logo} enableBorder={false} title="Faster Programmer" description="致力于提升程序猿👨‍💻👩‍💻的开发效率"/>
       </header>
+      <main id="main-content">
+          <Row>
+              <Col span={8}>
+                  <Card image={keymap} keyword="快捷键" title="使👨‍💻👩‍💻更快" description="帮助你熟练地使用各种快捷键，放弃鼠标和触控板吧，像黑客们那样"/>
+              </Col>
+              <Col span={8}>
+                  <Card image={script} keyword="脚本" title="使👨‍💻👩‍💻更快" description="你还在UI上费劲地点点点吗？简单的一条命令，替你轻松完成操作"/>
+              </Col>
+              <Col span={8}>
+                  <Card image={tools} keyword="工具" title="使👨‍💻👩‍💻更快" description="优秀的、便捷的工具，不仅能快速实现你的需求，还能节省下时间去做些有意义的事"/>
+              </Col>
+          </Row>
+      </main>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import './Card.css';
+
+class Card extends Component {
+    displayBorder = (event) => {
+        if (this.props.enableBorder !== false) {
+            event.target.style.border = "solid 1px gray";
+            event.target.style.borderRadius = "5px";
+        }
+    };
+
+    undisplayBorder = (event) => {
+        if (this.props.enableBorder !== false) {
+            event.target.style.border = "none";
+        }
+    };
+
+    render() {
+        return (
+            <div className="mdc-card" onMouseOver={this.displayBorder} onMouseOut={this.undisplayBorder}>
+                <section className="mdc-card__media">
+                    <img src={this.props.image} alt="card logo"/>
+                </section>
+                <section className="mdc-card__primary">
+                    <h1 className="mdc-card__title mdc-card__title--large">{this.props.keyword ? <span>{this.props.keyword}</span> : ""}{this.props.title}</h1>
+                </section>
+                <section className="mdc-card__supporting-text">
+                    {this.props.description}
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Card;
